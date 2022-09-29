b1e4f063cb1428080ccd0e4f2dd3fb4167294748
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-09-29 09:50:45 -0500

    second commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+
+  private apiKey: string = 'iKj1pu9Nms1d8z8EOaA1YLSersYZbH3D';
+  private servicioUrl : string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultado')!) || [];
+    //if(localStorage.getItem('historial')){
+    //this._historial = JSON.parse(localStorage.getItem('historial')!);
+    //}
+  }
+  buscarGifs(query: string = '') {
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params})
+      .subscribe((resp) => {
+        this.resultados = resp.data;
+        localStorage.setItem('resultado', JSON.stringify(this.resultados));
+      });
+  }
+}
